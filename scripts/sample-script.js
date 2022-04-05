06595aef92dc0421bfca3f0911d0c4f320099316
Apollon-Star <apollon71star@gmail.com>
2022-04-05 12:18:06 +0900

    deploy script

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,32 +1,70 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+// // We require the Hardhat Runtime Environment explicitly here. This is optional
+// // but useful for running the script in a standalone fashion through `node <script>`.
+// //
+// // When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// // Runtime Environment's members available in the global scope.
+// const hre = require("hardhat");
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+// async function main() {
+//   // Hardhat always runs the compile task when running scripts with its command
+//   // line interface.
+//   //
+//   // If this script is run directly using `node` you may want to call compile
+//   // manually to make sure everything is compiled
+//   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+//   // We get the contract to deploy
+//   const Greeter = await hre.ethers.getContractFactory("Greeter");
+//   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+//   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
-}
+//   console.log("Greeter deployed to:", greeter.address);
+// }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+
+
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("Blog", async function () {
+  it("Should create a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog")
+    const blog = await Blog.deploy("My blog")
+    await blog.deployed()
+    await blog.createPost("My first post", "12345")
+
+    const posts = await blog.fetchPosts()
+    expect(posts[0].title).to.equal("My first post")
+  })
+
+  it("Should edit a post", async function () {
+    const Blog = await ethers.getContractFactory("Blog")
+    const blog = await Blog.deploy("My blog")
+    await blog.deployed()
+    await blog.createPost("My Second post", "12345")
+
+    await blog.updatePost(1, "My updated post", "23456", true)
+
+    posts = await blog.fetchPosts()
+    expect(posts[0].title).to.equal("My updated post")
+  })
+
+  it("Should add update the name", async function () {
+    const Blog = await ethers.getContractFactory("Blog")
+    const blog = await Blog.deploy("My blog")
+    await blog.deployed()
+
+    expect(await blog.name()).to.equal("My blog")
+    await blog.updateName('My new blog')
+    expect(await blog.name()).to.equal("My new blog")
+  })
+})
